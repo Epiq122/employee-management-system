aed561fa4a531094963dd307878aea3dc0d2e76c
Robert Gleason <l2ob122@gmail.com>
2023-08-30 16:17:18 -0700

    added axios, added EmployeeService.js, implemented to we can see the users from the db

--- ems-frontend/src/components/ListEmployeeComponent.jsx
+++ ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -1,50 +1,43 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {listEmployees} from "../services/EmployeeService.js";
 
 const ListEmployeeComponent = () => {
-  const dummyData = [
-    {
-      id: 1,
-      firstName: "Rob",
-      lastName: "Gleason",
-      email: "admin@email.com",
-    },
-    {
-      id: 2,
-      firstName: "Bucky",
-      lastName: "Ohair",
-      email: "thebucky@email.com",
-    },
-    {
-      id: 3,
-      firstName: "Hans",
-      lastName: "Groober",
-      email: "dh1@email.com",
-    },
-  ];
-  return (
-    <div className="container">
-      <h2 className="text-center">List of Employees</h2>
-      <table className="table table-striped table-bordered">
-        <thead>
-          <tr>
-            <th>Employee Id</th>
-            <th>Employee First Name</th>
-            <th>Employee Last Name</th>
-            <th>Employee Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {dummyData.map((employee) => (
-            <tr key={employee.id}>
-              <td>{employee.id}</td>
-              <td>{employee.firstName}</td>
-              <td>{employee.lastName}</td>
-              <td>{employee.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+        listEmployees()
+            .then((response) => {
+                setEmployees(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
+    return (
+        <div className="container">
+            <h2 className="text-center">List of Employees</h2>
+            <table className="table table-striped table-bordered">
+                <thead>
+                <tr>
+                    <th>Employee Id</th>
+                    <th>Employee First Name</th>
+                    <th>Employee Last Name</th>
+                    <th>Employee Email</th>
+                </tr>
+                </thead>
+                <tbody>
+                {employees.map((employee) => (
+                    <tr key={employee.id}>
+                        <td>{employee.id}</td>
+                        <td>{employee.firstName}</td>
+                        <td>{employee.lastName}</td>
+                        <td>{employee.email}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
 };
 export default ListEmployeeComponent;
