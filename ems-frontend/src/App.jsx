28241b9a2b1fd4d282352d2a750b1ad64f30fa81
Robert Gleason <l2ob122@gmail.com>
2023-08-30 16:35:22 -0700

    added rouring from the ListEmployeeComponent

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -3,14 +3,21 @@ import ListEmployeeComponent from "./components/ListEmployeeComponent.jsx";
 import HeaderComponent from "./components/HeaderComponent.jsx";
 import FooterComponent from "./components/FooterComponent.jsx";
 
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
 function App() {
-  return (
-    <div>
-      <HeaderComponent />
-      <ListEmployeeComponent />
-      <FooterComponent />
-    </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <HeaderComponent/>
+                <Routes>
+                    <Route path="/" element={<ListEmployeeComponent/>}/>
+                    <Route path="/employees" element={<ListEmployeeComponent/>}/>
+                </Routes>
+                <FooterComponent/>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
