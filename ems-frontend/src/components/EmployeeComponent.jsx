82ebed45f8fb8be14ef301a018c38f0b75e2877d
Robert Gleason <l2ob122@gmail.com>
2023-09-02 08:24:24 -0700

    added logic for form validation in the employee component

--- ems-frontend/src/components/EmployeeComponent.jsx
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -1,75 +1,124 @@
-import {useState} from "react";
-import {createEmployee} from "../services/EmployeeService.js";
-import {useNavigate} from "react-router-dom";
+import { useState } from "react";
+import { createEmployee } from "../services/EmployeeService.js";
+import { useNavigate } from "react-router-dom";
 
 const EmployeeComponent = () => {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
 
-    const navigator = useNavigate();
-    const handleFirstName = (e) => setFirstName(e.target.value);
-    const handleLastName = (e) => setLastName(e.target.value);
-    const handleEmail = (e) => setEmail(e.target.value);
+  const navigator = useNavigate();
+  const handleFirstName = (e) => setFirstName(e.target.value);
+  const handleLastName = (e) => setLastName(e.target.value);
+  const handleEmail = (e) => setEmail(e.target.value);
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        const employee = {firstName, lastName, email};
+  const [errors, setErrors] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
 
-        createEmployee(employee).then((response) => {
-            console.log(response.data);
-            navigator('/employees')
-        });
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (validateForm()) {
+      const employee = { firstName, lastName, email };
+
+      createEmployee(employee).then((response) => {
+        console.log(response.data);
+        navigator("/employees");
+      });
+    }
+  }
+
+  function validateForm() {
+    let valid = true;
+    const errorsCopy = { ...errors };
+
+    if (firstName.trim()) {
+      errorsCopy.firstName = "";
+    } else {
+      errorsCopy.firstName = "First name is required";
+      valid = false;
+    }
+    if (lastName.trim()) {
+      errorsCopy.lastName = "";
+    } else {
+      errorsCopy.lastName = "Last name is required";
+      valid = false;
+    }
+    if (email.trim()) {
+      errorsCopy.email = "";
+    } else {
+      errorsCopy.email = "Email is required";
+      valid = false;
     }
 
-    return (
-        <div className="container">
-            <div className="row mt-4">
-                <div className="card col-md-6 offset-md-3">
-                    <h2 className="text-center">Add Employee</h2>
-                    <div className="card-body">
-                        <form action="">
-                            <div className="form-group mb-2">
-                                <label className="form-label">First Name: </label>
-                                <input
-                                    type="text"
-                                    placeholder="Enter Employee First Name"
-                                    name="firstName"
-                                    value={firstName}
-                                    className="form-control"
-                                    onChange={handleFirstName}
-                                />
-                            </div>
-                            <div className="form-group mb-2">
-                                <label className="form-label">Last Name: </label>
-                                <input
-                                    type="text"
-                                    placeholder="Enter Employee Last Name"
-                                    name="lastName"
-                                    value={lastName}
-                                    className="form-control"
-                                    onChange={handleLastName}
-                                />
-                            </div>
-                            <div className="form-group mb-2">
-                                <label className="form-label">Email: </label>
-                                <input
-                                    type="email"
-                                    placeholder="Enter Employee Email Address"
-                                    name="email"
-                                    value={email}
-                                    className="form-control"
-                                    onChange={handleEmail}
-                                />
-                            </div>
-                            <button className="btn btn-success" onClick={handleSubmit}>
-                                Submit
-                            </button>
-                        </form>
-                    </div>
-                </div>
-            </div>
+    setErrors(errorsCopy);
+    return valid;
+  }
+
+  return (
+    <div className="container">
+      <div className="row mt-4">
+        <div className="card col-md-6 offset-md-3">
+          <h2 className="text-center">Add Employee</h2>
+          <div className="card-body">
+            <form action="">
+              <div className="form-group mb-2">
+                <label className="form-label">First Name: </label>
+                <input
+                  type="text"
+                  placeholder="Enter Employee First Name"
+                  name="firstName"
+                  value={firstName}
+                  className={`form-control ${
+                    errors.firstName ? "is-invalid" : ""
+                  }`}
+                  onChange={handleFirstName}
+                />
+                {errors.firstName && (
+                  <div className="invalid-feedback">{errors.firstName}</div>
+                )}
+              </div>
+              <div className="form-group mb-2">
+                <label className="form-label">Last Name: </label>
+                <input
+                  type="text"
+                  placeholder="Enter Employee Last Name"
+                  name="lastName"
+                  value={lastName}
+                  className={`form-control ${
+                    errors.lastName ? "is-invalid" : ""
+                  }`}
+                  onChange={handleLastName}
+                />
+                {errors.lastName && (
+                  <div className="invalid-feedback">{errors.lastName}</div>
+                )}
+              </div>
+              <div className="form-group mb-2">
+                <label className="form-label">Email: </label>
+                <input
+                  type="email"
+                  placeholder="Enter Employee Email Address"
+                  name="email"
+                  value={email}
+                  className={`form-control ${errors.email ? "is-invalid" : ""}`}
+                  onChange={handleEmail}
+                />
+                {errors.email && (
+                  <div className="invalid-feedback">{errors.email}</div>
+                )}
+              </div>
+              <button className="btn btn-success" onClick={handleSubmit}>
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 export default EmployeeComponent;
