74d166a5f49112de57bc22b1850b9bcfe9737f25
Robert Gleason <l2ob122@gmail.com>
2023-09-04 16:06:52 -0700

    made table and dummy data for the department

--- ems-frontend/src/components/ListDepartmentComponent.jsx
+++ ems-frontend/src/components/ListDepartmentComponent.jsx
@@ -1,4 +1,42 @@
+import { useState } from "react";
+
 const ListDepartmentComponent = () => {
-  return <div>ListDepartmentComponent</div>;
+  let dummyData = [
+    {
+      id: 1,
+      departmentName: "IT",
+      departmentDescription: "Information Technology",
+    },
+    {
+      id: 2,
+      departmentName: "JAN",
+      departmentDescription: "Janitorial",
+    },
+  ];
+
+  const [departments, setDepartments] = useState(dummyData);
+
+  return (
+    <div>
+      <table className="table table-striped table-bordered">
+        <thead>
+          <tr>
+            <th>Department Dd</th>
+            <th>Department Name</th>
+            <th>Department Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {departments.map((department) => (
+            <tr key={department.id}>
+              <td>{department.id}</td>
+              <td>{department.departmentName}</td>
+              <td>{department.departmentDescription}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 export default ListDepartmentComponent;
